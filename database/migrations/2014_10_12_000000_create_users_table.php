fce67c68b1d5694eaa66d259a720dd761b19aeb0
Shaima <chaimaa.achi2@gmail.com>
2025-12-02 21:07:03 +0100

    update for migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,97 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        Schema::create('etudent', function (Blueprint $table) {
+             Schema::create('planning', function (Blueprint $table) {
+            $table->id_planning->primary();
+            
+        });
+            $table->id_etd()->primary();
             $table->string('name');
+            $table->string('prenom');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->date('date_naissance');
+            $table->string('niveau');
+            $table->string('filiere');
+            $table->integer('groupe');
+            $table->id_planning;
+        });
+        
+        Schema::create('enseignant', function (Blueprint $table) {
+            $table->id_eng()->primary();
+            $table->string('name');
+            $table->string('prenom');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('filiere');
+            $table->id_planning;
+          
+        });
+        Schema::create('responsable', function (Blueprint $table) {
+            $table->id_respo()->primary();
+            $table->string('name');
+            $table->string('prenom');
+            $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+        });
+        Schema::create('chef departement', function (Blueprint $table) {
+            $table->id_chef()->primary();
+            $table->string('name');
+            $table->string('prenom');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        Schema::create('module', function (Blueprint $table) {
+            $table->id_module()->primary();
+            $table->string('name');
+            $table->string('libelle');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        
+        Schema::create('examen', function (Blueprint $table) {
+            $table->id_exam()->primary();
+            $table->string('name');
+            $table->string('type');
+            $table->string('statut');
+            $table->id_module;
+            $table->timestamps();
+            $table->id_planning;
+        });
+        Schema::create('salle', function (Blueprint $table) {
+            $table->id_salle()->primary();
+            $table->string('name');
+            $table->string('localisation');
+            $table->boolean('disponibilite');
+            $table->id_exam;
+            $table->id_respo;
+            $table->timestamps();
+        });
+          Schema::create('passer', function (Blueprint $table) {
+            $table->id_salle->id_etd->id_exam->primary();
+
+        });
+        Schema::create('surveiller', function (Blueprint $table) {
+            $table->id_eng->id_etd->primary();
+            
+        });
+      
+        Schema::create('consulter', function (Blueprint $table) {
+            $table->id_planning->id_etd->primary();
+            $table->string('type');
+            
+            
+        });
+          Schema::create('notification', function (Blueprint $table) {
+            $table->id_chef->id_etd->id_eng->id_respo->primary();
+            $table->string('status');
+            
+            
         });
     }
 
